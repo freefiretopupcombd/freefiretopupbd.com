4eeaae5d3af1494e76592dd5da8e90d1746ceaef
freefiretopupbd.com <freefiretopupbd009@gmail.com>
2026-04-25 22:06:44 +0600

    fix roobot.ts file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -12,6 +12,7 @@ export default function robots(): MetadataRoute.Robots {
         '/profile/',
         '/api/',
         '/checkout/',
+        '/backend/',
         '/*?page=',
         '/*&page=',
       ],
