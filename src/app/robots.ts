3eeb30545d78c64f88d2a7f9fc7bb7d6ec6fcc0f
freefiretopupbd.com <freefiretopupbd009@gmail.com>
2026-04-27 10:02:24 +0600

    fix some error

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -12,7 +12,7 @@ export default function robots(): MetadataRoute.Robots {
         '/profile/',
         '/api/',
         '/checkout/',
-        '/gpay/',
+        '/backend/',
         '/*?page=',
         '/*&page=',
       ],
