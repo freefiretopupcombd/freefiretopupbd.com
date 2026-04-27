2ad2081d2bda533f58fa8bfd2186db884ea7b773
freefiretopupbd.com <freefiretopupbd009@gmail.com>
2026-04-27 15:53:33 +0600

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -47,6 +47,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/faq`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
