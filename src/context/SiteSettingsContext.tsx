a4a5cec4643c40ac5381b5045e79894f32216d4d
freefiretopupbd.com <freefiretopupbd009@gmail.com>
2026-04-27 15:48:59 +0600

    fix site settings

--- src/context/SiteSettingsContext.tsx
+++ src/context/SiteSettingsContext.tsx
@@ -20,13 +20,14 @@ interface SiteSettings {
 
 const SiteSettingsContext = createContext<SiteSettings | null>(null);
 
+
 export function SiteSettingsProvider({ children }: { children: React.ReactNode }) {
     const [siteSettings, setSiteSettings] = useState<SiteSettings | null>(null);
 
     useEffect(() => {
         async function fetchSiteSettings() {
             try {
-                const response = await axios.get(`/api/frontend-settings`);
+                const response = await axios.get("https://backend.freefiretopupbd.com/api/frontend-settings");
                 setSiteSettings(response.data.site_settings);
             } catch (error) {
                 console.error('Failed to fetch site settings:', error);
